36bc1ae51f7f10d2c7873fa169b3e29a8f26522d
Kazumasa Utashiro <kaz@utashiro.com>
2021-05-06 08:28:22 +0900

    minil new App::ansicode

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::ansicode - It's new $module
+
+# SYNOPSIS
+
+    use App::ansicode;
+
+# DESCRIPTION
+
+App::ansicode is ...
+
+# LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kazumasa Utashiro <kaz@utashiro.com>
---
+++ lib/App/ansicode.pm
@@ -0,0 +1,39 @@
+package App::ansicode;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::ansicode - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::ansicode;
+
+=head1 DESCRIPTION
+
+App::ansicode is ...
+
+=head1 LICENSE
+
+Copyright (C) Kazumasa Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kazumasa Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
